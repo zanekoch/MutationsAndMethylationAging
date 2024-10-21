f1cdc6dd7aaf7a03138acc97fcd4bccd75534773
zanekoch <zanehkoch@gmail.com>
2024-10-21 11:04:21 -0700

    gzip cpg locs

--- download_data/download_internal.sh
+++ download_data/download_internal.sh
@@ -3,3 +3,6 @@ curl -L -o data.zip "https://figshare.com/ndownloader/files/49904031"
 # unzip
 unzip data.zip
 # move data to the MutationsAndMethylationAging directory, such that MutationsAndMethylationAging/data exists if it does not already 
+
+# gunzip the cpg location file, which had to be compressed to commit to github
+gunzip ../dependency_files/illumina_cpg_450k_locations.csv.gz
